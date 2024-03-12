c55a42909fb7a80744cfbaaade586035889ae109
Antorkarmokar28 <antorkarmokar28@gmail.com>
2024-03-12 03:01:50 -0700

    module2-practice.js file data object added

--- module2-practice.js
+++ module2-practice.js
@@ -70,3 +70,76 @@ const people2 = [
 ];
 const getAgeValueTotalSum2 = people2.reduce((accumuletor, currentValue) => accumuletor + currentValue.age, 0);
 console.log(getAgeValueTotalSum2);
+
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary: [
+                    {school_name: 'ABC primary school'},
+                    {location: 'Peters burg'},
+                ]
+            },
+            {
+                secondary: [
+                    {school_name: 'ABC secondary school'},
+                    {location: 'St Lorence'},
+                ]
+            }
+        ]
+    }
+}
+console.log(data.Sophia.study[1].secondary[1].location);
+
+let students = {
+    2222: {
+        name: 'Jack',
+        section: 'C',
+        class: 'IX',
+        address: {
+            'building no': 12,
+            'street': 'St. Jonson',
+            'city': 'Petersburg',
+            'country': 'UK'
+        }
+    },
+    3333: {
+        name: 'Herry',
+        section: 'D',
+        class: 'X',
+        address: {
+            'building no': 85,
+            'street': 'DC road',
+            'city': 'Kochukhet',
+            'country': 'Bangladesh'
+        }
+    }
+}
+console.log(students['2222'].address['city']);
+console.log(students['3333'].name);
+
+let data2 = {
+    data:[
+        {
+            bookId: 1,
+            bookDetails: {
+                name: 'habluder adda',
+                category: 'XYZ',
+                price: '20$',
+            },
+            bookCategory: 'Basic',
+        },
+        {
+            bookId: 2,
+            bookDetails: {
+                name: 'gobluder adda',
+                category: 'ABC',
+                price: '40$',
+            },
+            bookCategory: 'Beginner',
+        },
+    ]
+}
+console.log(data2.data[0].bookDetails.name);
+console.log(data2.data[1].bookCategory);
