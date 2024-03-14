07b1e0be87e277778d5e63c095842cab8ab70a89
Antorkarmokar28 <antorkarmokar28@gmail.com>
2024-03-14 03:17:01 -0700

    destructuring-tips.js file some code added

--- destructuring-tips.js
+++ destructuring-tips.js
@@ -5,11 +5,12 @@ const person = {
 // const {name} = person;
 const {name:myName} = person;
 console.log(myName);
-const createSentence = (obj) =>{
-    const {name, age} = obj;
+const createSentence = ({name, age}) =>{
+    // const {name, age} = obj;
     // const sentence = `my name is ${obj.name}. I'm a ${obj.age} years older`;
+    const sentence = `my name is ${name}. I'm a ${age} years older`;
     // console.log(sentence);
-    console.log(name, age);
+    console.log(sentence);
 }
 // console.log(createSentence(person));
 createSentence(person);
